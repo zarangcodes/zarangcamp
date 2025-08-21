ccc8fa81d3ac0df03571e4af43a09c7671cec823
zarangcodes <dityasenop@gmail.com>
2025-08-22 05:03:29 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { HeroSection } from "@/components/hero-section"
 import { AboutSection } from "@/components/about-section"
 import { InitiativesSection } from "@/components/initiatives-section"
